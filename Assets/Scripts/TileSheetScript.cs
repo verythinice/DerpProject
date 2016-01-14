2cbb73036a69334da967b80361718cb4e063abd3
verythinice <quake.grizzly@gmail.com>
2016-01-14 13:44:11 -0800

    Finished procedural tile generation

---
+++ Assets/Scripts/TileSheetScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSheetScript : MonoBehaviour {
+    public int xSize;
+    public int ySize;
+    public int sheetSize;
+
+
+	// Use this for initialization
+	void Awake () {
+	    
+	}
+	
+
+}
