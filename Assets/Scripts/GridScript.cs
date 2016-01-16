cf78d513997ed779767e047b7c210e53c772ef79
verythinice <quake.grizzly@gmail.com>
2016-01-15 18:00:37 -0800

    Added separate file loading capability

--- Assets/Scripts/GridScript.cs
+++ Assets/Scripts/GridScript.cs
@@ -21,9 +21,9 @@ public class GridScript : MonoBehaviour {
         vertices = new Vector3[((xSize) * (ySize)*4)];
         int[] triangles = new int[xSize * ySize * 6];
         Vector2[] uv = new Vector2[vertices.Length];
-        for (int i=0, y=0, ti=0; y<ySize; y++)
+        for (int i=0, y=ySize-1, ti=0, qi=0; y>=0; y--)
         {
-            for (int x=0; x< xSize; x++, i+=4, ti+=6)
+            for (int x=0; x< xSize; x++, i+=4, ti+=6, qi++)
             {
                 vertices[i] = new Vector3(x, y);
                 vertices[i + 1] = new Vector3(x, y+1);
@@ -33,10 +33,11 @@ public class GridScript : MonoBehaviour {
                 triangles[ti + 3] = triangles[ti + 2] = i + 1;
                 triangles[ti + 4] = triangles[ti + 1] = i + 3;
                 triangles[ti + 5] = i + 2;
-                uv[i] = new Vector2(0, 7.0f/8);
-                uv[i + 1] = new Vector2(0, 1);
-                uv[i + 2] = new Vector2(1.0f / 8, 1);
-                uv[i + 3] = new Vector2(1.0f / 8, 7.0f / 8);
+                Vector2[] corners= GetComponent<TileSheetScript>().getTile(qi);
+                uv[i] = new Vector2(corners[0].x, corners[1].y);
+                uv[i + 1] = new Vector2(corners[0].x, corners[0].y);
+                uv[i + 2] = new Vector2(corners[1].x, corners[0].y);
+                uv[i + 3] = new Vector2(corners[1].x, corners[1].y);
             }
         }
         mesh.vertices = vertices;
--- Assets/Scripts/TileSheetScript.cs
+++ Assets/Scripts/TileSheetScript.cs
@@ -1,16 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class TileSheetScript : MonoBehaviour {
+public class TileSheetScript : MonoBehaviour
+{
     public int xSize;
     public int ySize;
-    public int sheetSize;
+    public int textureSize;
+    public TextAsset level;
+    private int[] tileMap;
 
+    // Use this for initialization
+    void Awake()
+    {
+        tileMap = new int[xSize * ySize];
+        string levelString = level.text;
+        string[] tiles = levelString.Split(new string[] { "\r\n", "\n", " " }, StringSplitOptions.None);
+        for (int i = 0; i < tileMap.Length; i++)
+        {
+            try {
+                tileMap[i] = Int32.Parse(tiles[i]);
+            }
+            catch (FormatException e)
+            {
+                tileMap[i] = (textureSize^2)-1;
+                Debug.LogWarning(e.ToString());
+                Debug.LogWarning(tiles[i]);
+            }
+        }
+    }
 
-	// Use this for initialization
-	void Awake () {
-	    
-	}
-	
-
+    public Vector2[] getTile(int tileNum)
+    {
+        return new Vector2[] {new Vector2((tileMap[tileNum]%textureSize)*(1.0f / textureSize), 1-((tileMap[tileNum]/textureSize)*1.0f/textureSize)), new Vector2(((tileMap[tileNum] % textureSize)*(1.0f/textureSize))+(1.0f/textureSize), 1 - ((tileMap[tileNum] / textureSize) * 1.0f / textureSize)- (1.0f / textureSize)) };
+    }
 }
